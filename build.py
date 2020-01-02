790f49592df4ff6a4e00bbfd3c41af18a1e2ea04
Pixelnarium <hello+pixelgithub@creepy-net.de>
2020-01-02 18:35:00 +0100

    Updates, add PPSSPP

--- build.py
+++ build.py
@@ -24,6 +24,7 @@ projects = [("com.dosbox.DOSBox","x86_64"),
         ("com.retrodev.blastem","x86_64"),
         ("org.mamedev.MAME","x86_64"),
         ("org.DolphinEmu.dolphin-emu","x86_64"),
+        ("org.ppsspp.PPSSPP","x86_64"),
         ("org.byuu.bsnes","x86_64")]
 
 
