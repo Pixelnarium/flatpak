482372d563e862bf8314368648a734c7a678802c
Pixelnarium <hello+pixelgithub@creepy-net.de>
2021-08-23 17:13:35 +0200

    Updates

--- build.py
+++ build.py
@@ -59,7 +59,7 @@ def main():
         else:
             name += ".yml"
 
-        ret = call(["flatpak-builder", build_dir, name, "--force-clean", "--repo="+repo_path, "--arch="+arch, "--state-dir="+stats_dir])#, "--keep-build-dirs"])
+        ret = call(["chrt", "-i", "0", "flatpak-builder", build_dir, name, "--force-clean", "--repo="+repo_path, "--arch="+arch, "--state-dir="+stats_dir])#, "--keep-build-dirs"])
         results.append(name + " -> " + str(ret))
 
         #shutil.rmtree("build", ignore_errors=True)
