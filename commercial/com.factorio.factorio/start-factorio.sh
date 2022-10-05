6c95e73d1b94792d023b5142f1dae905bea4e3c4
Pixelnarium <hello+pixelgithub@creepy-net.de>
2022-10-05 10:45:48 +0200

    Updates'n'Stuff

--- commercial/com.factorio.factorio/start-factorio.sh
+++ commercial/com.factorio.factorio/start-factorio.sh
@@ -4,7 +4,7 @@ if [ -e /var/config/config.ini ]
 then
   echo "OK"
 else
-  echo "; version=5" > /var/config/config.ini
+  echo "; version=9" > /var/config/config.ini
   echo "[path]" >> /var/config/config.ini
   echo "read-data=/app/factorio/data/" >> /var/config/config.ini
   echo "write-data=/var/data/factorio" >> /var/config/config.ini
